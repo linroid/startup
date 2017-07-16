3eacbd0d0022b4869d86eb68421ececb7ff84731
linroid <linroid@gmail.com>
2017-07-17 00:59:35 +0800

    refactor package

--- app/src/androidTest/java/com/linroid/gassist/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/linroid/gassist/ExampleInstrumentedTest.kt
@@ -1,4 +1,4 @@
-package com.linroid.startup
+package com.linroid.gassist
 
 import android.support.test.InstrumentationRegistry
 import android.support.test.runner.AndroidJUnit4
--- app/src/main/kotlin/com/linroid/gassist/App.kt
+++ app/src/main/kotlin/com/linroid/gassist/App.kt
@@ -1,4 +1,4 @@
-package com.linroid.startup
+package com.linroid.gassist
 
 import android.app.Application
 
